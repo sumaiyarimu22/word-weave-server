e3a099e08ddc452c2d49b68827fc172809be52b1
sumaiyarimu22 <sumaiyarimu22@gmail.com>
2023-12-21 17:04:37 +0600

    email validation done

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 import bcrypt from "bcrypt";
+import { nanoid } from "nanoid";
+
+//schema below
+import User from "./Schema/User.js";
 
 const server = express();
 let PORT = 3000;
@@ -14,6 +18,17 @@ server.use(express.json());
 mongoose.connect(process.env.DB_LOCATION, {
   autoIndex: true,
 });
+
+const generateUsername = async (email) => {
+  let username = email.split("@")[0];
+
+  let isUsernameNotUnique = await User.exists({
+    "personal_info.username": username,
+  }).then((result) => result);
+
+  isUsernameNotUnique ? (username += nanoid().substring(0, 5)) : "";
+  return username;
+};
 
 server.post("/signup", (req, res) => {
   let { fullname, email, password } = req.body;
@@ -36,7 +51,26 @@ server.post("/signup", (req, res) => {
     });
   }
 
-  return res.status(200).json({ status: "ok" });
+  bcrypt.hash(password, 10, async (err, hashed_password) => {
+    let username = await generateUsername(email);
+    let user = new User({
+      personal_info: { fullname, email, password: hashed_password, username },
+    });
+
+    user
+      .save()
+      .then((u) => {
+        return res.status(200).json({ user: u });
+      })
+      .catch((err) => {
+        if (err.code === 11000) {
+          return res.status(500).json({ error: "Email already exists" });
+        }
+
+        return res.status(500).json({ error: err.message });
+      });
+  });
+  // return res.status(200).json({ status: "ok" });
 });
 
 server.listen(PORT, () => {
